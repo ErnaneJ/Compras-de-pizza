a90f140f9ea782ac566b07f8ffcb517946a89216
Ernane <64796733+ErnaneJ@users.noreply.github.com>
2021-02-02 00:19:44 -0300

    Update pizzas.js

--- pizzas.js
+++ pizzas.js
@@ -8,5 +8,8 @@ let pizzaJson = [
     { id: 4, name: 'Americana', img: 'images/pizza4.png', price: 19.77, sizes: ['320g', '530g', '860g'], description: 'Pizza boa, pizza deliciosa! pizza que depois que você come, o corpo inteiro se renova' },
     { id: 5, name: 'Sorvete', img: 'images/pizza5.png', price: 21.43, sizes: ['320g', '530g', '860g'], description: 'Pizza boa, pizza deliciosa! pizza que depois que você come, o corpo inteiro se renova' },
     { id: 6, name: 'Moda da Casa', img: 'images/pizza6.png', price: 18.55, sizes: ['320g', '530g', '860g'], description: 'Pizza boa, pizza deliciosa! pizza que depois que você come, o corpo inteiro se renova' },
-    { id: 7, name: 'Chocolate', img: 'images/pizza7.png', price: 22.36, sizes: ['320g', '530g', '860g'], description: 'Pizza boa, pizza deliciosa! pizza que depois que você come, o corpo inteiro se renova' }
+    { id: 7, name: 'Chocolate', img: 'images/pizza7.png', price: 22.36, sizes: ['320g', '530g', '860g'], description: 'Pizza boa, pizza deliciosa! pizza que depois que você come, o corpo inteiro se renova' },
+    { id: 8, name: 'Peperonni', img: 'images/pizza2.png', price: 27.36, sizes: ['320g', '530g', '860g'], description: 'Pizza boa, pizza deliciosa! pizza que depois que você come, o corpo inteiro se renova' },
+    { id: 9, name: 'Frango com Catupiry', img: 'images/pizza4.png', price: 30.36, sizes: ['320g', '530g', '860g'], description: 'Pizza boa, pizza deliciosa! pizza que depois que você come, o corpo inteiro se renova' }
+
 ];
